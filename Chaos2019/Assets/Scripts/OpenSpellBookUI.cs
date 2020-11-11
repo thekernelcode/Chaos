89937a77747ec3d6426e15c92f28187ef9d72dc7
thekernelcode <45094334+thekernelcode@users.noreply.github.com>
2020-11-11 10:31:05 +0000

    Added Spellbook UI

---
+++ Chaos2019/Assets/Scripts/OpenSpellBookUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenSpellBookUI : MonoBehaviour
+{
+    public GameObject spellBookUIPanel;
+    bool isActive;
+
+    public void OpenandClose()
+    {
+        if (isActive == false)
+        {
+            spellBookUIPanel.transform.gameObject.SetActive(true);
+            isActive = true;
+        }
+        else 
+        {
+            spellBookUIPanel.transform.gameObject.SetActive(false);
+            isActive = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
